2f27326c7eabb364a4e49fd7fb22f0390c3ef11a
whokegard <rewelox@hotmail.com>
2022-09-28 10:04:07 +0200

    Added Album classes.

---
+++ src/main/java/com/example/ljudio/service/AlbumService.java
@@ -0,0 +1,60 @@
+package com.example.ljudio.service;
+
+import com.example.ljudio.dao.AlbumDAO;
+import com.example.ljudio.model.Album;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class AlbumService {
+
+    private final AlbumDAO albumDAO;
+
+    public Album addAlbum(Album newAlbum) {
+        Album album = findBySpotifyId(newAlbum.getSpotify_id());
+
+        if (album != null) {
+            return album;
+        }
+        return albumDAO.save(newAlbum);
+    }
+
+    private Album findBySpotifyId(String spotifyId) {
+        return getAllAlbums().stream()
+                .filter(album -> album.getSpotify_id().equalsIgnoreCase(spotifyId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    private List<Album> getAllAlbums() {
+        return (List<Album>) albumDAO.findAllAlbums();
+    }
+
+    public Album getAlbumById(long albumId) {
+        return albumDAO.findAlbumById(albumId).orElse(null);
+    }
+
+    public void deleteAlbumById(long albumId) {
+        Optional<Album> maybeAlbum = albumDAO.findAlbumById(albumId);
+        if (maybeAlbum.isEmpty())
+            return;
+        Album album = maybeAlbum.get();
+
+        albumDAO.deleteAlbumById(album.getAlbum_id());
+    }
+
+    public Album getAlbumBySpotifyId(long spotifyId) {
+        return albumDAO.findAlbumBySpotifyId(spotifyId).orElse(null);
+    }
+
+    public Album getAlbumByName(String name) {
+        return albumDAO.findArtistByName(name).orElse(null);
+    }
+
+    public Album getArtistByAlbum(String artist) {
+        return albumDAO.findArtistByAlbum(artist).orElse(null);
+    }
+}
