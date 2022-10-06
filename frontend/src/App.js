364a49c2e133330d364eaab22aed52858ac378d3
whokegard <rewelox@hotmail.com>
2022-10-06 14:07:13 +0200

    Finished header, changed location of logos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
+import "./css/Header.css";
 
 import AuthService from "./services/auth.service";
 
@@ -10,7 +11,7 @@ import Register from "./components/register.component";
 import Profile from "./components/profile.component";
 import BoardUser from "./components/board-user.component";
 import BoardAdmin from "./components/board-admin.component";
-import AddEmployeeComponent from './components/AddEmployeeComponent';
+import AddEmployeeComponent from "./components/AddEmployeeComponent";
 
 import EventBus from "./common/EventBus";
 
@@ -42,77 +43,70 @@ const App = () => {
   };
 
   return (
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <Link to={"/"} className="navbar-brand">
-            Ljudio
+    <div>
+      <Link to={"/"} className="header"></Link>
+      <li className="nav-item">
+        <Link to={"/home"} className="nav-link">
+          Home
+        </Link>
+      </li>
+
+      {showAdminBoard && (
+        <li className="nav-item">
+          <Link to={"/admin"} className="nav-link">
+            Admin Board
           </Link>
-          <div className="navbar-nav me-auto">
-            <li className="nav-item">
-              <Link to={"/home"} className="nav-link">
-                Home
-              </Link>
-            </li>
+        </li>
+      )}
 
-            {showAdminBoard && (
-                <li className="nav-item">
-                  <Link to={"/admin"} className="nav-link">
-                    Admin Board
-                  </Link>
-                </li>
-            )}
+      {currentUser && (
+        <li className="nav-item">
+          <Link to={"/user"} className="nav-link">
+            PLAY
+          </Link>
+        </li>
+      )}
 
-            {currentUser && (
-                <li className="nav-item">
-                  <Link to={"/user"} className="nav-link">
-                    PLAY
-                  </Link>
-                </li>
-            )}
-          </div>
-
-          {currentUser ? (
-              <div className="navbar-nav ms-auto">
-                <li className="nav-item">
-                  <Link to={"/profile"} className="nav-link">
-                    {currentUser.username}
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <a href="/login" className="nav-link" onClick={logOut}>
-                    LogOut
-                  </a>
-                </li>
-              </div>
-          ) : (
-              <div className="navbar-nav ms-auto">
-                <li className="nav-item">
-                  <Link to={"/login"} className="nav-link">
-                    Login
-                  </Link>
-                </li>
-
-                <li className="nav-item">
-                  <Link to={"/register"} className="nav-link">
-                    Sign Up
-                  </Link>
-                </li>
-              </div>
-          )}
-        </nav>
-
-        <div className="container mt-3">
-          <Routes>
-            <Route path="/login" element={<Login/>} />
-            <Route path="/register" element={<Register/>} />
-            <Route path="/profile" element={<Profile/>} />
-            <Route path="/user" element={<BoardUser/>} />
-            <Route path="/admin" element={<BoardAdmin/>} />
-            <Route path ="/edit-employee/:id" element={<AddEmployeeComponent/>}/>
-          </Routes>
+      {currentUser ? (
+        <div className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <Link to={"/profile"} className="nav-link">
+              {currentUser.username}
+            </Link>
+          </li>
+          <li className="nav-item">
+            <a href="/login" className="nav-link" onClick={logOut}>
+              LogOut
+            </a>
+          </li>
         </div>
+      ) : (
+        <div className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <Link to={"/login"} className="nav-link">
+              Login
+            </Link>
+          </li>
 
+          <li className="nav-item">
+            <Link to={"/register"} className="nav-link">
+              Sign Up
+            </Link>
+          </li>
+        </div>
+      )}
+
+      <div className="container mt-3">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/user" element={<BoardUser />} />
+          <Route path="/admin" element={<BoardAdmin />} />
+          <Route path="/edit-employee/:id" element={<AddEmployeeComponent />} />
+        </Routes>
       </div>
+    </div>
   );
 };
 
