8dc5179f2d962f0e06475a4959a4b6de3caae20a
whokegard <rewelox@hotmail.com>
2022-10-11 12:24:10 +0200

    Created Create a playlist page. Also updated footer button hover.

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,26 +1,32 @@
-import fetch from 'unfetch';
+import fetch from "unfetch";
 
-const checkStatus = response => {
-    if (response.ok) {
-        return response;
-    }
+const checkStatus = (response) => {
+  if (response.ok) {
+    return response;
+  }
 
-    const error = new Error(response.statusText);
-    error.response = response;
-    response.json().then(e => {
-        error.error = e;
-    });
-    return Promise.reject(error);
-}
+  const error = new Error(response.statusText);
+  error.response = response;
+  response.json().then((e) => {
+    error.error = e;
+  });
+  return Promise.reject(error);
+};
 
-    export const search = (title, artist) =>
-        fetch(`/api/spotify/${title}/${artist}`)
-            .then(checkStatus);
+export const search = (title, artist) =>
+  fetch(`/api/spotify/${title}/${artist}`).then(checkStatus);
 
-    export const track = songAPIId =>
-        fetch(`/api/spotify/${songAPIId}`)
-            .then(checkStatus);
+export const track = (songAPIId) =>
+  fetch(`/api/spotify/${songAPIId}`).then(checkStatus);
 
-    export const album = albumAPIId =>
-        fetch(`/api/spotify/album/${albumAPIId}`)
-            .then(checkStatus);
+export const album = (albumAPIId) =>
+  fetch(`/api/spotify/album/${albumAPIId}`).then(checkStatus);
+
+export const createAPlaylist = (userId, playlistId) =>
+  fetch(`/api/users/${userId}/playlist/${playlistId}/addPlaylist`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method: "PUT",
+    body: JSON.stringify(userId),
+  }).then(checkStatus);
